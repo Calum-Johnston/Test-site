037c74018eb8aa613797a7235468f58d58a4f9ce
Calum-Johnston <calumphilipjohnston@gmail.com>
2019-01-29 15:49:38 +0000

    Fixed port in index.js (process and env was wrong way wrong)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = env.process.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 app.listen(port, function(){
 	console.log('App listening on port 8090');
